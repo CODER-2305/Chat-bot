dd1fe6bcc9a08aee6967dbef41434797fb629b21
Uzcoin404 <uzcointg@gmail.com>
2022-01-19 20:59:34 +0500

    test

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+ob_start();
+define("API_KEY", "5046375589:AAGr3BvYHe2NiIPMYyzOpJalcdP2UyvC81Y");
+
+function bot ($method, $datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/$method";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
+    $res = curl_exec($ch);
+    if (curl_error($ch)) {
+        var_dump(curl_error($ch));
+    } else {
+        return json_decode($res);
+    }
+}
+
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$chat_id = $message->chat->id;
+$message_id = $message->message_id;
+$text = $message->text;
+
+if ($text == "/start") {
+    bot('sendMessage', [
+        'chat_id' => $chat_id,
+        'message_id' => $message_id,
+        'parse_node' => 'markdown',
+        'text' => "Botga xush kelibsiz"
+    ]);
+}
+
+ob_end_flush();
+?>
